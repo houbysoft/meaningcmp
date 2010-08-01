fa7bd65f786b5238dbbab2b77279f77dbf4838e2
Jan Dlabal <admin@houbysoft.com>
2010-08-01 19:33:28 -0400

    Wrote compare()

--- argmatch.py
+++ argmatch.py
@@ -8,9 +8,12 @@ from nltk.corpus import wordnet as wn
 
 
 class Model():
-    def __init__(self,els):
-        self.els = els
-        self.args = filter(lambda el : el[0]=='`',els)
+    def __init__(self,sentence):
+        self.els = self.tokenize(sentence)
+        self.args = filter(lambda el : el[0]=='`',self.els)
+
+    def tokenize(self,s):
+        return s.split(" ")
 
     def contains(self,el,relpos):
         if el=='`':
@@ -25,20 +28,29 @@ class Model():
                 return False
 
 
-def compare(a,b):
-    return a == b # TODO
+def compare(a,b,min=0.31):
+# returns True if a has equal meaning to b, False otherwise
+    asyn = wn.synsets(a)
+    bsyn = wn.synsets(b)
+    if len(asyn) > 0 and len(bsyn) > 0:
+        for ax in asyn:
+            if len(filter(lambda x : x == True,map(lambda bx : wn.path_similarity(ax,bx) > min, bsyn)))>0:
+                return True
+        return False
+    else:
+        return a == b
 
 
 def argmatch(model,tomatch_):
-    tomatch = list(tomatch_)
+    tomatch = nltk.word_tokenize(tomatch_)
     pos = -1
-    lastr = False
+    lastr = True
     for i in range(0,len(tomatch)):
         if model.contains(tomatch[i],pos+1):
             pos += 1
             tomatch[i] = False
             lastr = True
-        elif model.contains('`',pos+1) or model.contains('`',pos):
+        elif model.contains('`',pos+1) or (model.contains('`',pos) and lastr == False):
             if lastr == True:
                 pos += 1
                 lastr = False
